25dbf77dae5e3d22a789d5aa83176aa422bc8b7f
LAGGOUNE Walid <walidlaggoune159@gmail.com>
2025-04-03 07:42:08 +0100

    Added comments to base methods

--- app/Services/ExchangeRateService.php
+++ app/Services/ExchangeRateService.php
@@ -13,6 +13,18 @@ class ExchangeRateService
         private ExchangeRateCacheService $exchangeRateCacheService
     ) {}
 
+    /**
+     * Retrieves the exchange rate between two currencies.
+     *
+     * This method first checks the cache for an existing exchange rate.
+     * If the rate is not cached, it fetches the latest rate from the API.
+     * If the API request fails, it logs the error and returns a default value.
+     * The fetched rate is then cached for future use.
+     *
+     * @param Currency $from The base currency.
+     * @param Currency $to The target currency.
+     * @return float The exchange rate from the base currency to the target currency.
+    */
     public function get(Currency $from, Currency $to): float
     {
         $cachedRate = $this->exchangeRateCacheService->get($from, $to);
@@ -20,7 +32,7 @@ class ExchangeRateService
             return $cachedRate;
         }
 
-        $response = Http::timeout(config('exchange.timeout'))->get(config('exchange.endpoint').$from->value);
+        $response = Http::timeout(config('exchange.timeout'))->get(config('exchange.endpoint') . $from->value);
 
         if ($response->failed()) {
             $this->logError($from, $to, $response);
@@ -37,10 +49,18 @@ class ExchangeRateService
             $this->exchangeRateCacheService->set($from, $to, $rate);
         });
     }
-
+    /**
+     * Retrieves the default exchange rate between two currencies.
+     *
+     * This method gets the default exchange rate from the config file
+     *
+     * @param Currency $from The base currency.
+     * @param Currency $to The target currency.
+     * @return float The default exchange rate from the base currency to the target currency.
+    */
     public function getDefaultValueOf(Currency $from, Currency $to): float
     {
-        return config("defaults.{$from->value}.{$to->value}", 0);
+        return config("exchange.defaults.{$from->value}.{$to->value}", 0);
     }
 
     public function logError(Currency $from, Currency $to, Response $response): void
